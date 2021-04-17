c6aefe625397183f871ed41a8e5b9a8aaabe015e
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2021-04-17 08:47:20 -0500

    solución bug en la lista del adapter

--- app/src/main/java/co/cedesistemas/cakeapp/ProductsAdapter.kt
+++ app/src/main/java/co/cedesistemas/cakeapp/ProductsAdapter.kt
@@ -2,7 +2,6 @@ package co.cedesistemas.cakeapp
 
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.cedesistemas.cakeapp.databinding.ItemProductBinding
@@ -18,7 +17,7 @@ class ProductsAdapter(val listProducts: List<ProductResponse>) : RecyclerView.Ad
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         binding = ItemProductBinding.inflate(inflater, parent, false)
-        return MyViewHolder(binding.root)
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -32,14 +31,14 @@ class ProductsAdapter(val listProducts: List<ProductResponse>) : RecyclerView.Ad
     }
 
 
-    inner class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    inner class MyViewHolder(val itemProductBinding: ItemProductBinding) : RecyclerView.ViewHolder(itemProductBinding.root) {
 
         fun bindItem(product: ProductResponse) {
-            binding.textViewItem.text = product.name
+            itemProductBinding.textViewItem.text = product.name
             val url = "$URL${product.image}"
-            Glide.with(itemView.context).load(url).into(binding.imageViewItem)
-            itemView.setOnClickListener {
-                view.context.startActivity(Intent(view.context, DetailProductActivity::class.java))
+            Glide.with(itemProductBinding.root.context).load(url).into(itemProductBinding.imageViewItem)
+            itemProductBinding.root.setOnClickListener {
+                itemProductBinding.root.context.startActivity(Intent(itemProductBinding.root.context, DetailProductActivity::class.java))
             }
         }
 
