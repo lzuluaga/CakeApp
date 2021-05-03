e035ad8c855db10be3da02359f19a65fc639b508
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2021-05-03 07:57:40 -0500

    creando skeleton para mostrar los productos

--- app/src/main/java/co/cedesistemas/cakeapp/ShowProductsActivity.kt
+++ app/src/main/java/co/cedesistemas/cakeapp/ShowProductsActivity.kt
@@ -9,8 +9,10 @@ import co.cedesistemas.cakeapp.databinding.ActivityShowProductsBinding
 import co.cedesistemas.cakeapp.listener.ListenerProduct
 import co.cedesistemas.cakeapp.models.ProductResponse
 import co.cedesistemas.cakeapp.persistence.LocalCakeRepository
+import com.ethanhua.skeleton.Skeleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.Exception
@@ -26,6 +28,7 @@ class ShowProductsActivity : AppCompatActivity(), ListenerProduct {
         binding = ActivityShowProductsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.recyclerViewProducts.layoutManager = GridLayoutManager(this@ShowProductsActivity, 2)
+        showSkeleton()
         localRepository = LocalCakeRepository(this@ShowProductsActivity)
 
         CoroutineScope(Dispatchers.Main).launch {
@@ -36,7 +39,9 @@ class ShowProductsActivity : AppCompatActivity(), ListenerProduct {
                     localRepository.insertProducts(internalProducts)
                     internalProducts
                 }
-
+                delay(3000)// solo para ambitos de que sepan que si se ve,
+                // como el servicio puede ser rápido no se alcanza a ver la transición,
+                // luego de que verifiquen borran o comentan esta línea
                 binding.recyclerViewProducts.adapter = ProductsAdapter(products, this@ShowProductsActivity)
 
             } catch (e: Exception) {
@@ -56,5 +61,17 @@ class ShowProductsActivity : AppCompatActivity(), ListenerProduct {
         startActivity(Intent(this, DetailProductActivity::class.java).apply {
             //putExtra("product", product)
         })
+    }
+
+    private fun showSkeleton() {
+        Skeleton.bind(binding.recyclerViewProducts)
+            .shimmer(true)
+            .angle(20)
+            .frozen(false)
+            .duration(1000)
+            .count(6)
+            .color(R.color.white_opaque)
+            .load(R.layout.skeleton_item_product)
+            .show()
     }
 }
